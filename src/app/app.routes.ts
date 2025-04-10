6c78675763474aef53a4b5acbe65141726c60e1e
realSlimPudge <komissaeov010806@gmail.com>
2025-04-10 09:59:40 +0300

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'products', component: ProductsPageComponent },
+];
